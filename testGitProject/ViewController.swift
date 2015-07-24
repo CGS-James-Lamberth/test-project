2ff2c6c1fd1c37b2d4475c5fcc5452ee383a40be
James Lamberth <17793@cgs.act.edu.au>
2015-07-24 12:53:04 +1000

    Initial commit

---
+++ testGitProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testGitProject
+//
+//  Created by James Lamberth on 24/07/2015.
+//  Copyright (c) 2015 James Lamberth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
