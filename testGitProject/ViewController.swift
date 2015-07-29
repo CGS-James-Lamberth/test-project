c62c1c4285b46e6578b5731ec5789e1e3e817cba
James Lamberth <17793@cgs.act.edu.au>
2015-07-29 11:23:58 +1000

    New Feature added

--- testGitProject/ViewController.swift
+++ testGitProject/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // New Feature kind average
         // Comment
         
+        for i ..10 {
+            println(i)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
